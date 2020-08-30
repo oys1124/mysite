6c0d139527b39c8d97967d3534a378ade5a68ca1
oys <1144166030@qq.com>
2020-08-30 14:14:09 +0800

    项目的第一次提交

---
+++ likes/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import LikeCount
+
+# Register your models here.
+@admin.register(LikeCount)
+class LikeCountAdmin(admin.ModelAdmin):
+    list_display = ('liked_num','content_object')
